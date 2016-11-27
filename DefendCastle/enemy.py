14d8c9256db85c9257f8e7f5ebc7c23aa000c0be
yan0909 <yan0909@naver.com>
2016-11-27 23:59:26 +0900

    11/27(일)
    노멀 적 함수, 변수이름 바꾸기

--- DefendCastle/enemy.py
+++ DefendCastle/enemy.py
@@ -3,31 +3,31 @@ import random
 import main_state
 
 class Normal_Enemy:            # Normal_Enemy 로 바꾸기
-    image = None
-    die_image = None
-    alive_image = None
+    running_image = None
+    dying_image = None
+    getting_up_image = None
     def __init__(self):
         self.x, self.y = 0,random.randint(146, 197)
-        self.first_y = self.y
-        self.last_y = None
+        self.starting_y = self.y
+        self.falling_started_y = None
         self.width = 50
         self.height = 75
-        self.speed = random.randint(1,2) / 2
-        if(Normal_Enemy.image == None):
-            self.image = load_image('resource/enemy01_animation.png')
-        if(Normal_Enemy.die_image == None):
-            self.die_image = load_image('resource/enemy01_die.png')
-        if(Normal_Enemy.alive_image == None):
-            self.alive_image = load_image('resource/enemy01_alive.png')
+        self.running_speed = random.randint(1,2) / 2
+        if(Normal_Enemy.running_image == None):
+            self.running_image = load_image('resource/enemy01_animation.png')
+        if(Normal_Enemy.dying_image == None):
+            self.dying_image = load_image('resource/enemy01_die.png')
+        if(Normal_Enemy.getting_up_image == None):
+            self.getting_up_image = load_image('resource/enemy01_alive.png')
         self.state = 0
-        self.frame = 0
-        self.die_frame = 0
-        self.alive_frame = 0
-        self.f_speed = 0
+        self.run_animation_frame = 0
+        self.die_animation_frame = 0
+        self.get_up_animation_frame = 0
+        self.falling_speed = 0
         self.isHit = False # 이 적이 성을 때렸는지 안때렸는지.
     def update(self):
         if(self.state == 0 or self.state == 1):
-            self.x += self.speed
+            self.x += self.running_speed
             if(self.y >= 146 and self.y < 160 and self.x >= 436):
                 self.x = 436
                 self.state = 1
@@ -41,34 +41,34 @@ class Normal_Enemy:            # Normal_Enemy 로 바꾸기
                 self.isHit = True
 
         elif(self.state == 3):
-            self.f_speed += 0.3
-            self.y -= self.f_speed
-            if(self.y <= self.first_y):
-                self.y = self.first_y
-                if(self.last_y >= 450):
+            self.falling_speed += 0.3
+            self.y -= self.falling_speed
+            if(self.y <= self.starting_y):
+                self.y = self.starting_y
+                if(self.falling_started_y >= 450):
                     self.state = 4
                 else:
                     self.state = 5
-                    self.alive_frame = 0
+                    self.get_up_animation_frame = 0
         elif(self.state == 4):
-            if(self.die_frame < 6 * 12):
-                self.die_frame += 1
+            if(self.die_animation_frame < 6 * 12):
+                self.die_animation_frame += 1
         elif(self.state == 5):
-            if(self.alive_frame >= 30):
+            if(self.get_up_animation_frame >= 30):
                 self.state = 0
-            self.alive_frame += 1
+            self.get_up_animation_frame += 1
 
         self.frame = (self.frame + 1) % (4 * 10)
 
     def draw(self):
         if(self.state == 0 or self.state == 2 or self.state == 3):
-            self.image.clip_draw(math.floor(self.frame / 10) % 4 * 50, 75, 50, 75, self.x, self.y)
+            self.running_image.clip_draw(math.floor(self.frame / 10) % 4 * 50, 75, 50, 75, self.x, self.y)
         elif(self.state == 1):
-            self.image.clip_draw(math.floor(self.frame / 10) % 4 * 50, 0, 50, 75, self.x, self.y)
+            self.running_image.clip_draw(math.floor(self.frame / 10) % 4 * 50, 0, 50, 75, self.x, self.y)
         elif(self.state == 4 and self.die_frame < 6 * 12):
-            self.die_image.clip_draw(math.floor(self.die_frame / 12) * 70, 0 ,70, 100, self.x, self.y)
+            self.dying_image.clip_draw(math.floor(self.die_frame / 12) * 70, 0 ,70, 100, self.x, self.y)
         elif(self.state == 5):
-            self.alive_image.clip_draw(math.floor(self.alive_frame / 10) * 70, 0, 70, 60, self.x, self.y)
+            self.getting_up_image.clip_draw(math.floor(self.alive_frame / 10) * 70, 0, 70, 60, self.x, self.y)
         #print(self.state, self.frame, self.frame % 4, self.frame % 5, self.x, self.y)
     pass
 
