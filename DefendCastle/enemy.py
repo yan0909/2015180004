0ff4f909afce78b5779888f4a95796f52a6357c6
yan0909 <yan0909@naver.com>
2016-11-28 23:59:38 +0900

    11/28(월)

--- DefendCastle/enemy.py
+++ DefendCastle/enemy.py
@@ -2,77 +2,101 @@ from pico2d import *
 import random
 import main_state
 
-class Normal_Enemy:            # Normal_Enemy 로 바꾸기
+class Enemy_Base:
     running_image = None
+    attacking_image = None
     dying_image = None
     getting_up_image = None
+
     def __init__(self):
-        self.x, self.y = 0,random.randint(146, 197)
+        self.x, self.y = 0, random.randint(146, 197)
         self.starting_y = self.y
         self.falling_started_y = None
-        self.width = 50
-        self.height = 75
-        self.running_speed = random.randint(1,2) / 2
-        if(Normal_Enemy.running_image == None):
-            self.running_image = load_image('resource/enemy01_animation.png')
-        if(Normal_Enemy.dying_image == None):
-            self.dying_image = load_image('resource/enemy01_die.png')
-        if(Normal_Enemy.getting_up_image == None):
-            self.getting_up_image = load_image('resource/enemy01_alive.png')
-        self.state = 0
-        self.run_animation_frame = 0
-        self.die_animation_frame = 0
-        self.get_up_animation_frame = 0
+
+        self.running_speed = random.randint(1, 2) / 2
+
+        self.state = 'running'  # [running, attacking, dragging, falling, dying, getting_up]
+        self.running_frame = 0
+        self.attacking_frame = 0
+        self.dying_frame = 0
+        self.getting_up_frame = 0
         self.falling_speed = 0
-        self.isHit = False # 이 적이 성을 때렸는지 안때렸는지.
+
+class Normal_Enemy(Enemy_Base):            # Normal_Enemy 로 바꾸기
+    def __init__(self):
+        self.running_width = 50
+        self.running_height = 75
+
+        self.attacking_width = 50
+        self.attacking_height = 75
+
+        self.dying_width = 70
+        self.dying_height = 100
+
+        self.getting_up_width = 70
+        self.getting_up_height = 60
+
+        if(Normal_Enemy.running_image == None):
+            self.running_image = load_image('resource/normal_enemy_running.png')
+        if (Normal_Enemy.attacking_image == None):
+            self.attacking_image = load_image('resource/normal_enemy_attacking.png')
+        if(Normal_Enemy.dying_image == None):
+            self.dying_image = load_image('resource/normal_enemy_dying.png')
+        if(Normal_Enemy.getting_up_image == None):
+            self.getting_up_image = load_image('resource/normal_enemy_getting_up.png')
+
     def update(self):
-        if(self.state == 0 or self.state == 1):
+        if(self.state == 'running'):
             self.x += self.running_speed
+            # self.frame = (self.frame + 1) % (4 * 10)
             if(self.y >= 146 and self.y < 160 and self.x >= 436):
                 self.x = 436
-                self.state = 1
+                self.state = 'attacking'
             if(self.y >= 160 and self.y <= 178 and self.x >= 420):
                 self.x = 420
-                self.state = 1
+                self.state = 'attacking'
             if(self.y > 178 and self.y <= 197 and self.x >= 404):
                 self.x = 404
-                self.state = 1
-            if(self.state == 1 and self.frame == 0):
-                self.isHit = True
+                self.state = 'attacking'
 
-        elif(self.state == 3):
+
+        elif(self.state == 'dragging'):
             self.falling_speed += 0.3
             self.y -= self.falling_speed
             if(self.y <= self.starting_y):
                 self.y = self.starting_y
                 if(self.falling_started_y >= 450):
-                    self.state = 4
+                    self.state = 'falling'
                 else:
-                    self.state = 5
-                    self.get_up_animation_frame = 0
-        elif(self.state == 4):
-            if(self.die_animation_frame < 6 * 12):
-                self.die_animation_frame += 1
-        elif(self.state == 5):
-            if(self.get_up_animation_frame >= 30):
+                    self.state = 'getting_up'
+                    self.getting_up_frame = 0
+        elif(self.state == 'dying'):
+            if(self.dying_frame < 6 * 12):
+                self.dying_frame += 1
+        elif(self.state == 'getting_up'):
+            if(self.getting_up_frame >= 30):
                 self.state = 0
-            self.get_up_animation_frame += 1
+            self.getting_up_frame += 1
 
-        self.frame = (self.frame + 1) % (4 * 10)
+
 
     def draw(self):
-        if(self.state == 0 or self.state == 2 or self.state == 3):
-            self.running_image.clip_draw(math.floor(self.frame / 10) % 4 * 50, 75, 50, 75, self.x, self.y)
-        elif(self.state == 1):
-            self.running_image.clip_draw(math.floor(self.frame / 10) % 4 * 50, 0, 50, 75, self.x, self.y)
-        elif(self.state == 4 and self.die_frame < 6 * 12):
-            self.dying_image.clip_draw(math.floor(self.die_frame / 12) * 70, 0 ,70, 100, self.x, self.y)
-        elif(self.state == 5):
-            self.getting_up_image.clip_draw(math.floor(self.alive_frame / 10) * 70, 0, 70, 60, self.x, self.y)
+        if(self.state == 'running' or self.state == 'dragging' or self.state == 'falling'):
+            self.running_image.clip_draw(math.floor(self.running_frame / 10) % 4 * 50, 75, self.running_width, self.running_height, self.x, self.y)
+        elif(self.state == 'attacking'):
+            self.attacking_image.clip_draw(math.floor(self.attacking_frame / 10) % 4 * 50, 0, self.attacking_width, self.attacking_height, self.x, self.y)
+        elif(self.state == 'dying' and self.dying_frame < 6 * 12):
+            self.dying_image.clip_draw(math.floor(self.dying_frame / 12) * 70, 0, self.dying_width, self.dying_height, self.x, self.y)
+        elif(self.state == 'getting_up'):
+            self.getting_up_image.clip_draw(math.floor(self.getting_up_frame / 10) * 70, 0, self.getting_up_width, self.getting_up_height, self.x, self.y)
         #print(self.state, self.frame, self.frame % 4, self.frame % 5, self.x, self.y)
+
+    def get_bb(self):
+        return self.x - self.running_width / 2, self.y - self.running_height / 2, self.x + self.running_width / 2, self.y + self.running_height / 2
+
     pass
 
-class Enemy02(Normal_Enemy):
+class Crush_Enemy(Enemy_Base):
     def __init__(self):
         Normal_Enemy.__init__()
         self.x, self.y = 0,random.randint(146, 197)
@@ -82,11 +106,13 @@ class Enemy02(Normal_Enemy):
         self.height = 75
         self.speed = random.randint(1,2) / 2
         if(Normal_Enemy.image == None):
-            self.image = load_image('resource/enemy02_animation.png')
+            self.image = load_image('resource/crush_enemy_running.png')
+        if (Normal_Enemy.attacking_image == None):
+            self.attacking_image = load_image('resource/crush_enemy_attacking.png')
         if(Normal_Enemy.die_image == None):
-            self.die_image = load_image('resource/enemy01_die.png')
+            self.die_image = load_image('resource/normal_enemy_dying.png')
         if(Normal_Enemy.alive_image == None):
-            self.alive_image = load_image('resource/enemy02_alive.png')
+            self.alive_image = load_image('resource/crush_enemy_getting_up.png')
         self.state = 0
         self.frame = 0
         self.die_frame = 0
@@ -103,7 +129,10 @@ class Enemy02(Normal_Enemy):
         elif(self.state == 5):
             self.alive_image.clip_draw(math.floor(self.alive_frame / 10) * 70, 0, 70, 60, self.x, self.y)
         #print(self.state, self.frame, self.frame % 4, self.frame % 5, self.x, self.y)
+    def get_bb(self):
+        return self.x - self.running_width / 2, self.y - self.running_height / 2, self.x + self.running_width / 2, self.y + self.running_height / 2
     pass
+
 
 class Giant_Enemy(Normal_Enemy):
     def __init__(self):
@@ -136,5 +165,8 @@ class Giant_Enemy(Normal_Enemy):
         elif (self.state == 5):
             self.alive_image.clip_draw(math.floor(self.alive_frame / 10) * 70, 0, 70, 60, self.x, self.y)
             # print(self.state, self.frame, self.frame % 4, self.frame % 5, self.x, self.y)
+
+    def get_bb(self):
+        return self.x - self.running_width / 2, self.y - self.running_height / 2, self.x + self.running_width / 2, self.y + self.running_height / 2
 
     pass
