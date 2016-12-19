2c73e7129320ab0a5bcd32316e98aba9cc6a5d0a
yan0909 <yan0909@naver.com>
2016-12-19 23:57:45 +0900

    12/19(월)
    Former-commit-id: 14ff703cbdcedd92956a9fd126fa675b4a73f1cd

--- DefendCastle/start_menu.py
+++ DefendCastle/start_menu.py
@@ -1,15 +1,21 @@
 from pico2d import *
+from sdl2.events import SDL_MOUSEMOTION
+
 import game_framework
 import main_state
 
 name = 'StartMenu'
 image = None
 logo_time = 0.0
-menu_sound = None
+menu_mouse_over_sound = None
+start_button_mouse_over = False
 
 def enter():
-    global image
+    global image, start_button_mouse_over, menu_mouse_over_sound
     image = load_image('resource/title.png')
+    menu_mouse_over_sound = load_music('sound/menu.mp3')
+    menu_mouse_over_sound.set_volume(90)
+    start_button_mouse_over = False
 
 def exit():
     global image
@@ -36,6 +42,7 @@ def resume():
     pass
 
 def handle_events():
+    global start_button_mouse_over, menu_mouse_over_sound
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -45,15 +52,19 @@ def handle_events():
                 exit()
             elif (event.type == SDL_MOUSEBUTTONDOWN):
                 if(354 < event.x and event.x < 535 and 370 < event.y and event.y < 400):
-                    menu_sound = load_music('sound/menu.mp3')
-                    menu_sound.set_volume(90)
-                    menu_sound.play()
+                    menu_mouse_over_sound.play()
                     game_framework.change_state(main_state)
 
                 if (342 < event.x and event.x < 535 and 163 < event.y and event.y < 188):
                     game_framework.change_state(main_state)
                 if(event.x >= 409 and event.x <= 476 and 599 - event.y >= 127 and 599 - event.y <= 154):
                     game_framework.quit()
+            elif(event.type == SDL_MOUSEMOTION):
+                if (start_button_mouse_over == False and 354 < event.x and event.x < 535 and 370 < event.y and event.y < 400):
+                    start_button_mouse_over = True
+                    menu_mouse_over_sound.play()
+                elif(start_button_mouse_over and not (354 < event.x and event.x < 535 and 370 < event.y and event.y < 400)):
+                    start_button_mouse_over = False
                 #mouse_x = event.x
                 #mouse_y = 599 - event.y
                 #print(mouse_x, mouse_y)
